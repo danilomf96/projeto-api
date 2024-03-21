1aa3e9f66cdbee0f3fa16dbd2bd7af5b2ccfda59
danilo <danilomferreira96@gmail.com>
2024-03-21 11:07:41 -0300

    Segundo commit

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,13 @@
+Console.Clear();
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+
+// Funcionalidades da aplicaçao --> EndPoints
+app.MapGet("/", () => "Localhost");
+app.MapGet("/produto", () => "Produtos");
+app.MapGet("/produto/listar", () => "Listagem de produtos");
+app.MapPost("/produto/cadastrar", () => "Cadastro de produtos");
 
 app.Run();
