429b3d51efbab26e97b107468562da649075c6a5
danilo <danilomferreira96@gmail.com>
2024-03-21 11:34:31 -0300

    commit instanciando objetos da lista e Exercicio para casa

--- API/Program.cs
+++ API/Program.cs
@@ -3,11 +3,24 @@ Console.Clear();
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+//criando array list e instanciando objeto
+List<Produto> produtos = new List<Produto>();
+produtos.Add(new Produto("Celular", "IOS"));
+produtos.Add(new Produto("Celular", "Android"));
+produtos.Add(new Produto("TV", "LG"));
+produtos.Add(new Produto("Placa de video", "NVIDIA"));
+
 
 // Funcionalidades da aplicaçao --> EndPoints
 app.MapGet("/", () => "Localhost");
 app.MapGet("/produto", () => "Produtos");
-app.MapGet("/produto/listar", () => "Listagem de produtos");
+
+
+app.MapGet("/produto/listar", () => produtos);
+
+// !EXERCICIO! <---- CADASTRAR PRODUTOS DENTRO DA LISTA ---> !EXERCICIO!
 app.MapPost("/produto/cadastrar", () => "Cadastro de produtos");
 
 app.Run();
+
+record Produto(string nome, string descricao);
