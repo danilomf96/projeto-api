e8256dab2929186d4d6976b51e374f3578b70f39
danilo <danilomferreira96@gmail.com>
2024-04-11 11:31:14 -0300

    Criaçao do banco de dados ----> !!!

---
+++ API/Models/AppDataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Models;
+
+//Classe que representa o EntityFrameworkCore : Code First
+public class AppDataContext : DbContext
+{
+    //Representação das classes que vao virar tabelas do banco de dados
+    public DbSet<Produto> Produtos { get; set; }
+
+    //override OnConfiguring
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        //Configuraçao da conexão com o banco de dados
+        optionsBuilder.UseSqlite("Data Source=app.db");
+
+    }
+}
